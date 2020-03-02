44041ea3a8fc8dbb5013fd6f7a371828a8962e38
Nurul Hassan <nurulhassan2000@gmail.com>
2020-03-03 00:48:45 +0530

    Kisanmart added

---
+++ controllers/homeController.js
@@ -0,0 +1,29 @@
+var bodyParser = require("body-parser");
+
+const home = function(req,res){
+    res.render("./../resources/views/index.ejs");
+}
+
+const checkout = function(req,res){
+    res.render("./../resources/views/checkout.ejs");
+}
+
+const cart = function(req,res){
+    res.render("./../resources/views/cart.ejs");
+}
+
+const productDetails = function(req,res){
+    res.render("./../resources/views/product-details.ejs");
+}
+
+const shop = function(req,res){
+    res.render("./../resources/views/shop.ejs");
+}
+
+module.exports = {
+    home,
+    checkout,
+    cart,
+    productDetails,
+    shop
+}
